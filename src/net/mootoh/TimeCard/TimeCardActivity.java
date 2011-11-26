8805e012c757034c13561d1590f71d341623c61b
mootoh <mootoh@gmail.com>
2011-11-26 13:35:44 -0800

    #4: displays elapsed time of the current tag.

--- src/net/mootoh/TimeCard/TimeCardActivity.java
+++ src/net/mootoh/TimeCard/TimeCardActivity.java
@@ -18,14 +18,34 @@ public final class TimeCardActivity extends NavigationActivity {
     public void onResume() {
         super.onResume();
 
+        displayCurrentTag();
+    }
+
+    private void displayCurrentTag() {
         TagStore tagStore = new TagStore(this);
         Tag currentTag = tagStore.currentTag();
         String tagInfo = null;
         if (currentTag != null) {
-            tagInfo = currentTag.name + ": last ON=" + currentTag.timeStamp.toString();
+            tagInfo  = currentTag.name;
+            tagInfo += ":  " + getElapsedTime(currentTag.timeStamp);
         }
 
         android.widget.TextView currentTagNameTextView = (android.widget.TextView)findViewById(R.id.currentTagName);
-        currentTagNameTextView.setText(currentTag == null ? "No Tag" : tagInfo);
+        currentTagNameTextView.setText(tagInfo == null ? "No Tag" : tagInfo);
+    }
+
+    private String getElapsedTime(java.util.Date currentDate) {
+        long lastTime = currentDate.getTime();
+        long curTime  = System.currentTimeMillis();
+        long elapsed = (curTime - lastTime);
+
+        long elapsedHour = elapsed / (60*60*1000);
+        elapsed -= elapsedHour * (60*60*1000);
+
+        long elapsedMin = elapsed / (60*1000);
+        elapsed -= elapsedMin * 60*1000;
+        long elapsedSec = elapsed / 1000;
+
+        return String.format("%02d:%02d:%02d", elapsedHour, elapsedMin, elapsedSec);
     }
 }
