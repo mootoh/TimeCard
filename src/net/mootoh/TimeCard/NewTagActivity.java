24b6ccb85126ecfff19fc33567fa633295d8699f
mootoh <mootoh@gmail.com>
2011-11-27 00:26:34 -0800

    added color buttons.

--- src/net/mootoh/TimeCard/NewTagActivity.java
+++ src/net/mootoh/TimeCard/NewTagActivity.java
@@ -4,30 +4,32 @@ import java.sql.SQLException;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public final class NewTagActivity extends NavigationActivity {
+   Button selected;
+
     @Override
     public void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.newtag);
         titleLabel.setText("New Tag");
 
         final String tagId = getIntent().getStringExtra("tagId");
-
-        // show a form to input the name or something for this tag
-        setContentView(R.layout.newtag);
-
         final EditText editText = (EditText)findViewById(R.id.tagNameEdit);
+
         navigationButton.setText("Save");
         navigationButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (editText.getText().equals(""))
+                if (editText.getText().toString().equals(""))
                     return;
 
                 TagStore tagStore = new TagStore(getApplicationContext());
 
                 try {
-                    tagStore.addTag(tagId, editText.getText().toString(), "#ff0000");
+                    String tagColor = (String)selected.getTag();
+                    tagStore.addTag(tagId, editText.getText().toString(), tagColor);
                 } catch (SQLException e) {
                     Log.e(getClass().getSimpleName(), "cannot add this tag:" + tagId);
                 }
@@ -35,5 +37,38 @@ public final class NewTagActivity extends NavigationActivity {
                 finish();
             }
         });
+
+        ColorButton[] colorButtons = {
+                new ColorButton((Button)findViewById(R.id.color1Button), "ff7700"),
+                new ColorButton((Button)findViewById(R.id.color2Button), "ff007f"),
+                new ColorButton((Button)findViewById(R.id.color3Button), "66ff66"),
+                new ColorButton((Button)findViewById(R.id.color4Button), "ffff66"),
+                new ColorButton((Button)findViewById(R.id.color5Button), "007fff"),
+        };
+
+        for (ColorButton colorButton : colorButtons) {
+            colorButton.button.setTag(colorButton.color);
+            colorButton.button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Log.d(getClass().getSimpleName(), "touched: ");
+                    // unselect current selected
+                    selected = (Button)v;
+                }
+            });
+        }
+        Button bbb = new Button(this);
+        bbb.setText("bbb");
+        LinearLayout ll = (LinearLayout)findViewById(R.id.colorButtonLayout);
+        ll.addView(bbb);
     };
+
+    class ColorButton {
+        public Button button;
+        public String color;
+
+        public ColorButton(Button button, String color) {
+            this.button = button;
+            this.color = color;
+        }
+    }
 }
